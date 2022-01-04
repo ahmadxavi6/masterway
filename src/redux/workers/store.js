acc8c34f173edc462b25f5c6d3df5d9845aa41e3
ahmad <ahmadxavi6@gmail.com>
2022-01-05 01:42:33 +0200

    Update store.js

--- src/redux/workers/store.js
+++ src/redux/workers/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const middleware = [thunk];
 
@@ -9,6 +10,6 @@ if (process.env.NODE_ENV === "development") {
   middleware.push(logger);
 }
 
-const store = createStore(rootReducer, applyMiddleware(...middleware));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)));
 
 export default store;
