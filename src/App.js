8c82315bc888be54f28af0bb668da5dcb22b016a
ahmad <ahmadxavi6@gmail.com>
2022-01-07 21:48:12 +0200

    added Map
    added map to the site with google maps api

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
-import './App.css';
-import SideBar from './components/SideBar';
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom" ;
-import Home from './pages/Home';
-import IncomeAndOutcome from './pages/IncomeAndOutcome'
-import Vechiles from './pages/Vechiles'
-import Workers from './pages/Workers'
-import Worker from './pages/Worker'
-import Schedule from './pages/Schedule'
-import React   from 'react';
+import "./App.css";
+import SideBar from "./components/SideBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import IncomeAndOutcome from "./pages/IncomeAndOutcome";
+import Vechiles from "./pages/Vechiles";
+import Workers from "./pages/Workers";
+import Worker from "./pages/Worker";
+import Schedule from "./pages/Schedule";
+import React from "react";
+import MapGps from "./pages/MapGps";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-
   return (
     <>
-    
-    <ToastContainer />
-    <Router>
-   <SideBar/>
-      <Switch>
-        <Route path="/" exact component ={Home} >
-          </Route>
-        <Route path="/vechiles"component ={Vechiles} >
-        </Route>
-        <Route exact path="/workers"component ={Workers} >
-        </Route>
-        <Route path="/workers/profile/"component ={Worker} >
-        </Route>
-        <Route path="/incomeAndOutcome"component ={IncomeAndOutcome} >
-        </Route>
-        <Route path="/schedule"component ={Schedule} >
-        </Route>
-      </Switch>
-   </Router>
-   </>
+      <ToastContainer />
+      <Router>
+        <SideBar />
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/vechiles" component={Vechiles}></Route>
+          <Route exact path="/workers" component={Workers}></Route>
+          <Route path="/workers/profile/" component={Worker}></Route>
+          <Route path="/incomeAndOutcome" component={IncomeAndOutcome}></Route>
+          <Route path="/schedule" component={Schedule}></Route>
+          <Route path="/map" component={MapGps}></Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
