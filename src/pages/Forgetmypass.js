8ac1a55533e3905e07cfe183714606996fe72c3d
ahmad <ahmadxavi6@gmail.com>
2022-01-17 23:10:32 +0200

    adding components
    adding a component to send mail to the admin email
     and adding component to reset the password

---
+++ src/pages/Forgetmypass.js
@@ -0,0 +1,52 @@
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+const API = "http://localhost:5000";
+
+function Forgetmypass() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const user = { email };
+    // send the username and password to the server
+    await axios
+      .patch(`${API}/forgetmypass`, user)
+      .then((resp) => {
+        alert("Reset Password Email sent");
+        document.getElementById("email1").value = "";
+      })
+      .catch((err) => console.log("Email not Found in Database"));
+  };
+
+  const [email, setEmail] = useState("");
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <form
+            className="card-body cardbody-color p-lg-5"
+            onSubmit={handleSubmit}
+          >
+            <div className="mb-3">
+              <input
+                type="email"
+                onChange={({ target }) => setEmail(target.value)}
+                className="form-control"
+                id="email1"
+                aria-describedby="emailHelp"
+                placeholder="Email"
+              />
+            </div>
+
+            <div className="text-center">
+              <button type="submit" className="btn btn-color px-5 mb-5 w-100">
+                Send My Password to Email
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Forgetmypass;
---
+++ src/pages/Passwordreset.js
@@ -0,0 +1,53 @@
+import React from "react";
+import axios from "axios";
+import { useState } from "react";
+const API = "http://localhost:5000";
+
+function Forgetmypass() {
+  const handleSubmit = async (e) => {
+    const id = window.location.pathname;
+    e.preventDefault();
+    const user = { password };
+    // send the username and password to the server
+    await axios
+      .post(`${API}${id}`, user)
+      .then((resp) => {
+        alert("Password has changed");
+        document.getElementById("password1").value = "";
+      })
+      .catch((err) => console.log("Email not Found in Database"));
+  };
+
+  const [password, setPassword] = useState("");
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <form
+            className="card-body cardbody-color p-lg-5"
+            onSubmit={handleSubmit}
+          >
+            <div className="mb-3">
+              <input
+                type="password"
+                onChange={({ target }) => setPassword(target.value)}
+                className="form-control"
+                id="password1"
+                aria-describedby="emailHelp"
+                placeholder="New Password"
+              />
+            </div>
+
+            <div className="text-center">
+              <button type="submit" className="btn btn-color px-5 mb-5 w-100">
+                Change password
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Forgetmypass;
