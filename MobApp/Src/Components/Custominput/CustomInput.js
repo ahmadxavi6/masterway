c75ee77a8241aad29bf3e0cd7c89cc3a24cacfcc
ahmad <ahmadxavi6@gmail.com>
2022-02-20 20:00:30 +0200

    update custiom inputs

--- MobApp/Src/Components/Custominput/CustomInput.js
+++ MobApp/Src/Components/Custominput/CustomInput.js
@@ -1,17 +1,46 @@
 import { View, Text, TextInput, StyleSheet } from "react-native";
 import React from "react";
-
-const CustomInput = ({ value, setValue, placeholder, secureTextEntry }) => {
+import { Controller } from "react-hook-form";
+const CustomInput = ({
+  control,
+  name,
+  rules = {},
+  placeholder,
+  secureTextEntry,
+}) => {
   return (
-    <View style={styles.container}>
-      <TextInput
-        value={value}
-        onChangeText={setValue}
-        style={styles.input}
-        placeholder={placeholder}
-        secureTextEntry={secureTextEntry}
-      ></TextInput>
-    </View>
+    <Controller
+      control={control}
+      name={name}
+      rules={rules}
+      render={({
+        field: { value, onChange, onBlur },
+        fieldState: { error },
+      }) => (
+        <>
+          <View
+            style={[
+              styles.container,
+              { borderColor: error ? "red" : "#e8e8e8" },
+            ]}
+          >
+            <TextInput
+              value={value}
+              onBlur={onBlur}
+              onChangeText={onChange}
+              placeholder={placeholder}
+              style={styles.input}
+              secureTextEntry={secureTextEntry}
+            ></TextInput>
+          </View>
+          {error && (
+            <Text style={{ color: "red", alignSelf: "stretch" }}>
+              {error.message || "error"}
+            </Text>
+          )}
+        </>
+      )}
+    />
   );
 };
 const styles = StyleSheet.create({
@@ -20,6 +49,7 @@ const styles = StyleSheet.create({
     width: "100%",
     borderColor: "#e8e8e8",
     borderWidth: 1,
+    height: 30,
     borderRadius: 5,
     paddingHorizontal: 10,
     marginVertical: 8,
