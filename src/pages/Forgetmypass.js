6d54229bbbd9e6bcd32fa0650d394ace47ef17de
ahmadxavi6 <74915093+ahmadxavi6@users.noreply.github.com>
2022-03-04 02:04:49 +0200

    Update Forgetmypass.js

--- src/pages/Forgetmypass.js
+++ src/pages/Forgetmypass.js
@@ -2,8 +2,7 @@ import React from "react";
 import axios from "axios";
 import { useState } from "react";
 import { toast } from "react-toastify";
-const API = "http://192.168.56.1:5000";
-
+const API = "https://masterway.herokuapp.com/";
 function Forgetmypass() {
   const handleSubmit = async (e) => {
     e.preventDefault();
